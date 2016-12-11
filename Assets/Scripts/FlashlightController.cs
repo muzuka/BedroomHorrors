1d3047359c6026a79bc899b8fac6ce3881bf3ce1
muzuka <seanbrown7935@gmail.com>
2016-12-10 21:19:19 -0700

    Flashlight can now rotate.

--- Assets/Scripts/FlashlightController.cs
+++ Assets/Scripts/FlashlightController.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class FlashlightController : MonoBehaviour {
 
 	Vector3 mousePos;
+	int screenMidWidth;
+	int screenMidHeight;
+
+	int offsetX;
+	int offsetY;
 
 	void Awake ()
 	{
 		mousePos = new Vector3();
+		screenMidWidth = Screen.width/2;
+		screenMidHeight = Screen.height/2;
 	}
 
 	void Update () 
 	{
 		mousePos = Input.mousePosition;
+		Debug.Log(mousePos);
+
+		offsetX = (int)mousePos.x - screenMidWidth;
+		offsetY = (int)mousePos.y - screenMidHeight;
+
+		gameObject.transform.rotation = Quaternion.Euler(-offsetY, offsetX, 0.0f);
 	}
 }
