a017005a466708597f348c07914b6f08df469c31
muzuka <seanbrown7935@gmail.com>
2016-12-10 18:00:03 -0700

    Added models for main level.

---
+++ Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightController : MonoBehaviour {
+
+	Vector3 mousePos;
+
+	void Awake ()
+	{
+		mousePos = new Vector3();
+	}
+
+	void Update () 
+	{
+		mousePos = Input.mousePosition;
+	}
+}
