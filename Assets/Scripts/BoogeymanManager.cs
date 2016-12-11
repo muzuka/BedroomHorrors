6fbf3bf3ca7c766c8c65043ad427966e91d605c7
muzuka <seanbrown7935@gmail.com>
2016-12-11 01:43:56 -0700

    moved code to level manager

--- Assets/Scripts/BoogeymanManager.cs
+++ Assets/Scripts/BoogeymanManager.cs
@@ -2,20 +2,107 @@
 
 public class BoogeymanManager : MonoBehaviour {
 
-	int level;
-
 	Vector3 frontOrigin;
 	Vector3 rightOrigin;
 	Vector3 leftOrigin;
+	Vector3 closetOrigin;
 
 	void Start () {
-		level = 1;
-		frontOrigin = new Vector3(-2.5f, -0.3f, -2.15);
+		frontOrigin = new Vector3(-2.5f, -0.3f, -2.15f);
 		rightOrigin = new Vector3(-3.0f, -0.3f, -2.75f);
 		leftOrigin = new Vector3(-3.0f, -0.3f, -1.75f);
+		closetOrigin = new Vector3(1.0f, 0.25f, -2.0f);
 	}
 
 	void Update () {
-		
+
+		if(GetComponent<LevelManager>().completed)
+		{
+			GetComponent<LevelManager>().completed = false;
+			switch(GetComponent<LevelManager>().level)
+			{
+			case 1:
+				levelOneActivate();
+				GetComponent<LevelManager>().subLevel++;
+				break;
+			case 2:
+				levelTwoActivate();
+				GetComponent<LevelManager>().subLevel++;
+				break;
+			case 3:
+				levelThreeActivate();
+				GetComponent<LevelManager>().subLevel++;
+				break;
+			}
+		}
+	}
+
+	void levelOneActivate()
+	{
+		switch(GetComponent<LevelManager>().subLevel)
+		{
+		case 1:
+			spawnBoogeyman(frontOrigin);
+			break;
+		case 2:
+			spawnBoogeyman(leftOrigin);
+			spawnBoogeyman(rightOrigin);
+			break;
+		case 3:
+			spawnBoogeyman(frontOrigin);
+			spawnBoogeyman(leftOrigin);
+			spawnBoogeyman(rightOrigin);
+			break;
+		}
+	}
+
+	void levelTwoActivate()
+	{
+		switch(GetComponent<LevelManager>().subLevel)
+		{
+		case 1:
+			spawnBoogeyman(leftOrigin);
+			spawnBoogeyman(rightOrigin);
+			break;
+		case 2:
+			spawnBoogeyman(frontOrigin);
+			spawnBoogeyman(leftOrigin);
+			spawnBoogeyman(rightOrigin);
+			break;
+		case 3:
+			spawnBoogeyman(frontOrigin);
+			spawnBoogeyman(leftOrigin);
+			spawnBoogeyman(rightOrigin);
+			break;
+		}
+	}
+
+	void levelThreeActivate()
+	{
+		switch(GetComponent<LevelManager>().subLevel)
+		{
+		case 1:
+			spawnBoogeyman(frontOrigin);
+			spawnBoogeyman(leftOrigin);
+			spawnBoogeyman(rightOrigin);
+			break;
+		case 2:
+			spawnBoogeyman(frontOrigin);
+			spawnBoogeyman(leftOrigin);
+			spawnBoogeyman(rightOrigin);
+			break;
+		case 3:
+			spawnBoogeyman(closetOrigin);
+			spawnBoogeyman(closetOrigin);
+			spawnBoogeyman(closetOrigin);
+			spawnBoogeyman(closetOrigin);
+			spawnBoogeyman(closetOrigin);
+			break;
+		}
+	}
+
+	void spawnBoogeyman(Vector3 origin)
+	{
+		Instantiate(Resources.Load("Prefabs/Boogeyman_Weak"), origin, Quaternion.identity);
 	}
 }
