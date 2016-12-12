5ba47641b866ca5e4d31a72a7b8c3572968f9ad6
muzuka <seanbrown7935@gmail.com>
2016-12-12 14:37:11 -0700

    Added ray casting.

--- Assets/Scripts/FlashlightController.cs
+++ Assets/Scripts/FlashlightController.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class FlashlightController : MonoBehaviour {
+
+	public UnityEvent deathEvent; 
 
 	Vector3 mousePos;
 	int screenMidWidth;
 	int screenMidHeight;
+
+	const float timeToKill = 0.1f;
+	float timeConsumed;
 
 	int offsetX;
 	int offsetY;
@@ -17,11 +23,14 @@ public class FlashlightController : MonoBehaviour {
 		mousePos = new Vector3();
 		screenMidWidth = Screen.width/2;
 		screenMidHeight = Screen.height/2;
+		timeConsumed = 0.0f;
 	}
 
 	void Update () 
 	{
 		mousePos = Input.mousePosition;
+
+		//shootRay();
 
 		offsetX = (int)mousePos.x - screenMidWidth;
 		offsetY = (int)mousePos.y - screenMidHeight;
@@ -45,5 +54,22 @@ public class FlashlightController : MonoBehaviour {
 		}
 
 		gameObject.transform.rotation = Quaternion.Euler(-offsetY, 90+offsetX, 0.0f);
+	}
+
+	void shootRay ()
+	{
+		if(Raycaster.shootMouseRay().transform != null)
+		{
+			Debug.Log("Hit boogeyman.");
+			timeConsumed += Time.deltaTime;
+			if(timeConsumed >= timeToKill)
+			{
+				deathEvent.Invoke();
+			}
+		}
+		else
+		{
+			timeConsumed = 0.0f;
+		}
 	}
 }
