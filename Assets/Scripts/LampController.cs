7e2e0c1981a741ca898d2ca57d1ba5b10a03d603
muzuka <seanbrown7935@gmail.com>
2016-12-11 17:08:10 -0700

    Added a bunch of models and a texture folder

---
+++ Assets/Scripts/LampController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampController : MonoBehaviour {
+
+	Light lampLight;
+
+	void Start ()
+	{
+		lampLight = GetComponent<Light>();
+	}
+
+	void Update () 
+	{
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			if(lampLight.intensity >= 1.0f)
+				lampLight.intensity = 0.0f;
+			else
+				lampLight.intensity = 1.1f;
+		}
+	}
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public Vector3 velocity;
+	public float speed;
+
+	float step;
+	float rotateStep;
+
+	Vector3 directionToDestination;
+	Vector3 destination;
+	Quaternion rotationToDestination;
+
+	void Start () 
+	{
+		rotateStep = 0.1f;
+		destination = Vector3.zero;
+		velocity = Vector3.Normalize(velocity) * speed;
+		directionToDestination = destination - gameObject.transform.position;
+		rotationToDestination = Quaternion.FromToRotation(velocity, directionToDestination);
+	}
+
+	void Update () 
+	{
+		destination = GetComponent<Boogeyman>().destination;
+		directionToDestination = destination - gameObject.transform.position;
+
+		gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, rotationToDestination, rotateStep);
+
+		moveBoogeyman();
+	}
+
+	void moveBoogeyman ()
+	{
+		velocity = Vector3.Normalize(velocity) * speed;
+		step = speed * Time.deltaTime;
+		gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, destination, step);
+	}
+}
