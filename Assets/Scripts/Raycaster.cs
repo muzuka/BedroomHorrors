a772251d856002195bb7b47b97bf44b2e0bd9f26
muzuka <seanbrown7935@gmail.com>
2016-12-12 14:32:54 -0700

    Added Raycaster from Beachwars

---
+++ Assets/Scripts/Raycaster.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class Raycaster {
+
+	public static Ray getRay () 
+	{
+		Vector3 mPos = Input.mousePosition;
+		return Camera.main.ScreenPointToRay(mPos);
+	}
+
+	public static RaycastHit shootMouseRay ()
+	{
+		RaycastHit hit;
+		Physics.Raycast(getRay(), out hit);
+		return hit;
+	}
+
+}
