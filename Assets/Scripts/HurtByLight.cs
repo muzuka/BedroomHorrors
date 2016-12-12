13112e2c0e01e191594cdafa8a8797d4ba13cb5e
muzuka <seanbrown7935@gmail.com>
2016-12-11 21:35:13 -0700

    Added trigger code but doesn’t work yet.

--- Assets/Scripts/HurtByLight.cs
+++ Assets/Scripts/HurtByLight.cs
@@ -1,16 +1,31 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class HurtByLight : MonoBehaviour {
 
+	int maxHealth;
 	int health;
 
-	void Start () {
-		health = 10;
+	void Start () 
+	{
+		maxHealth = 10;
+		health = maxHealth;
 	}
 
-	void Update () {
+	void Update () 
+	{
 		if(health <= 0)
 			Destroy(gameObject);
+	}
+
+	void OnTriggerStay ()
+	{
+		Debug.Log("Light is on boogeyman.");
+		health--;
+	}
+
+	void OnTriggerExit ()
+	{
+		Debug.Log("Light has left the boogeyman.");
+		health = maxHealth;
 	}
 }
